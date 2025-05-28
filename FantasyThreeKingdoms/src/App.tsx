52d2b66be936ab9a2888ae72c3b2d0891700f721
Chanz-He <linkfind.hecz@gmail.com>
2025-05-28 21:58:15 +0800

    fix : Update

--- FantasyThreeKingdoms/src/App.tsx
+++ FantasyThreeKingdoms/src/App.tsx
@@ -1,9 +1,32 @@
-import './index.css'
+import './index.scss'
 
 function App() {
 
   return (
-   <div className='box'></div>
+    <div className='top'>
+      <div className='title'><img src="src/images/title.png" alt="" /></div>
+      <div>
+        <div className='android'><img src="src/images/android.png" alt="" /></div>
+        <div className='apple'><img src="src/images/apple.png" alt="" /></div>
+      </div>
+      <div className='exclusive'>
+        <div><img src="src/images/Exclusive.png" alt="" /></div>
+        <div className='exclusiveMain'>
+          <div className='r99999'><img src="src/images/R99999.png" alt="" /></div>
+          <div className='receive'>仅限领一次</div>
+          <div className='invitationCode'><img src="src/images/InvitationCode.png" alt="" /></div>
+          <div><img src="src/images/gift.png" alt="" /></div>
+          <div className='gift'>礼包包含自动战斗*1、自动补给*1、背包扩容*3、内测专员套装*1</div>
+          <div className='giftPack'>
+            <div className='giftPackWord'>如何领取礼包</div>
+            <div><img src="src/images/giftPack1.png" alt="" /></div>
+            <div><img src="src/images/giftPack2.png" alt="" /></div>
+            <div><img src="src/images/giftPack3.png" alt="" /></div>
+            <div><img src="src/images/giftPack4.png" alt="" /></div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
